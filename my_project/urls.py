6e5d62c1cbffccbe9ff0cd8c98f3a3df937842dc
Sara mehlis <Sara.mehlis@gmail.com>
2024-03-04 19:21:16 +0000

    add whitenoise and setup css file

--- my_project/urls.py
+++ my_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('market.urls')),
-    path('accounts/', include('allauth.urls')),
+    path('', include('market.urls')),
+    path('account/', include('allauth.urls')),
 ]
