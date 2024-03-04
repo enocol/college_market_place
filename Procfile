5d73e7f79c9a20128f9ceec9e8ffc8782921e900
Sara mehlis <Sara.mehlis@gmail.com>
2024-03-04 21:55:38 +0000

    correct Procfile content

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn market.wsgi
+web: gunicorn my_project.wsgi
--- my_project/settings.py
+++ my_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
@@ -88,12 +89,17 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DEBUG" in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DB_URL"))
+    }
 
 
 # Password validation
