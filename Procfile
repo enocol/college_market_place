311ff81c0ca06e193536cfc1dd9ba15d2108cc4c
Sara mehlis <Sara.mehlis@gmail.com>
2024-03-04 20:15:49 +0000

    add models for market

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn kevintest.wsgi
+web: gunicorn market.wsgi
--- market/models.py
+++ market/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Market(models.Model):
+    title = models.CharField(max_length=100)
+    image = CloudinaryField('image', default='placeholder')
