eda9307a164bf5283068c581dd449afb8981d166
enocol <eno.col@gmail.com>
2024-03-05 02:37:50 +0000

    Edit settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -19,7 +19,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = BASE_DIR / 'templates'
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 
 
 DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DB_URL"))
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 
 
